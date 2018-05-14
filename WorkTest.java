489aed52baba055d2c5a2667acdff67db1f966ed
MedalSUN <2496667187@qq.com>
2018-05-14 11:03:31 +0800

    补课——判断，分支

---
+++ WorkTest.java
@@ -0,0 +1,126 @@
+package ccec.edu;
+
+import java.util.Scanner;
+
+public class WorkTest {
+
+	public static void main(String[] args) {
+
+		/*int i=0;
+		//int f=0;这个位置不对。
+		while (i < 4)
+		{
+			int f=0;
+			while (f < 8)
+			{
+				System.out.print("*");
+				f++;
+			}
+			System.out.println();
+			i++;
+		}
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("请输入成绩");
+		int i=scanner.nextInt();
+		if (i >= 90 && i<= 100) {
+			System.out.println("A");
+		}else if (i >= 80 && i< 90) {
+			System.out.println("B");
+		}else if (i >= 70 && i < 80) {
+			System.out.println("C");
+		}else if (i >= 60 && i < 70){
+			System.out.println("D");
+		}else if (i>= 0 && i < 60){
+			System.out.println("E");
+		}else {
+			System.out.println("输入为不合法的成绩");
+		}
+		
+		
+		
+		int grade = 70;
+		switch (grade) {
+			case 1+1 ://switch不能使用区间。
+				System.out.println("");
+			 	break;
+		}
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("请输入会员号（<四位整数>）：");
+		int num = scan.nextInt();
+		System.out.println("请输入会员生日：");
+		String date = scan.next();//注意next()和nextLine（）区别，第二个无法正确得到输入数值。
+		System.out.println("请输入积分：");
+		int grade = scan.nextInt();
+		if (num>999 && num < 10000) {
+			System.out.println("已录入的信息为：");
+			System.out.print(num);
+			System.out.print("\t"+date);
+			System.out.print("\t"+grade);
+		}else {
+			System.out.println("录入信息失败");
+		}
+		
+		
+		int sum=0;
+		for(int i=1;i<=100;i++)
+		{
+			//sum=sum+i;
+			sum += i;
+		}
+		System.out.println(sum);
+		
+		
+		
+		int sum=0;
+		for(int i=0;i<=100;i += 2)
+		{
+			//sum=sum+i;
+			sum += i;
+		}
+		System.out.println(sum);
+		
+		
+		int sum=0;
+		for(int i=1;i<=100;i++)
+		{
+			//sum=sum+i;
+			if (i%2==0) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
+		
+		
+		int i=0;
+		int sum=0;
+		while (i<=100){
+			sum +=i;
+			i++;
+		}
+		System.out.println(sum);
+		
+		
+		
+		
+		int i=0;
+		int sum=0;
+		do {
+			sum +=i;
+			i++;
+		} while (i<=100);
+		System.out.println(sum);
+		
+		
+		
+		
+		
+		
+		
+	*/
+		
+		
+		
+	} 
+}
